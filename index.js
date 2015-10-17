cdc6976cd544928a46cc33a185424d31d0f23b44
nda86 <nda86@mail.ru>
2015-10-17 20:43:06 +0800

    add sqlite

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database_name.db');
+
+
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
@@ -10,10 +14,25 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app.post('/getGoods', function (req, res){
 	var code = req.body.code;
+	db.run("INSERT into table_name(col1,col2,col3) VALUES (val1,val2,val3)");
 	console.log(code);
 	res.send("Вы отправили: " + code)
 });
 
 app.listen(1337, function (){
 	console.log('Listening');
-})
+});
+
+
+//Perform SELECT Operation
+// db.all("SELECT * from blah blah blah where this="+that,function(err,rows){
+//rows contain values while errors, well you can figure out.
+// });
+
+//Perform INSERT operation.
+
+//Perform DELETE operation
+// db.run("DELETE * from table_name where condition");
+
+//Perform UPDATE operation
+// db.run("UPDATE table_name where condition");
